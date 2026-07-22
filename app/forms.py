7150960924488b7758aefa1f88a037be4a556c15
Salato <simonyamongo@gmail.com>
2026-07-22 12:53:08 +0300

    Added followers feature and included unit test for the all the features added so far

--- app/forms.py
+++ app/forms.py
@@ -48,4 +48,9 @@ class EditProfileForm(FlaskForm):
             user = db.session.scalar(sa.select(User).where(
                 User.username == username.data)) 
             if user is not None:
-                raise ValidationError('Please use a different username.')            
+                raise ValidationError('Please use a different username.')  
+
+
+### Empty form for following and unfollowing
+class EmptyForm(FlaskForm):
+    submit = SubmitField('submit')                      
